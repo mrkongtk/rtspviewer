28a265017ed62c3bc64c18ed9c391db40cabd8f7
Kan Kong <dev@mrkongtk.com>
2026-02-28 09:13:06 +0000

    docs: add documentation to DeviceInfo
    - Add KDoc to common DeviceInfo interface
    - Add KDoc to Android implementation
    - Add KDoc to iOS implementation

--- shared/src/iosMain/kotlin/com/mrkongtk/rtspviewer/shared/data/DeviceInfoIosImpl.kt
+++ shared/src/iosMain/kotlin/com/mrkongtk/rtspviewer/shared/data/DeviceInfoIosImpl.kt
@@ -3,7 +3,10 @@ package com.mrkongtk.rtspviewer.shared.data
 import platform.Foundation.NSBundle
 
 class DeviceInfoIosImpl : DeviceInfo {
+    /**
+     * Retrieves the bundle identifier from the main application bundle.
+     * Returns "unknown" if the identifier cannot be resolved.
+     */
     override val bundleId: String
         get() = NSBundle.mainBundle.bundleIdentifier ?: "unknown"
-
 }
